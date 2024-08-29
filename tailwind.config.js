c876b578435cac1bc1b731a922a25d3b74047245
aliakbarmotallebi <motallebi.aliakbar@gmail.com>
2024-08-29 04:03:44 -0700

    start project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: "tw-",
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        iransans: ["IRANSans"],
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ tailwindcss.bat
@@ -0,0 +1,3 @@
+echo before
+CMD /C tailwindcss -i ./styles/tailwind.css -o ./styles/output.css --watch
+echo after
